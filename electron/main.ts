71158d09cba457ac2cf65d31562ef8ce7be5c7c1
todak-sun <tjsdydwn@gmail.com>
2023-02-20 20:58:29 +0900

    electron hot reloader 설정추가. tailwindcss 설정추가.

--- electron/main.ts
+++ electron/main.ts
@@ -1,6 +1,8 @@
 import * as path from "path";
 import { app, BrowserWindow } from "electron";
+import installExtension, { REACT_DEVELOPER_TOOLS } from "electron-devtools-installer";
 import * as isDev from "electron-is-dev";
+import electronReload from "electron-reload";
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -12,27 +14,42 @@ const createWindow = () => {
   });
 
   if (isDev) {
-    win.loadURL(`file://${__dirname}/../index.html`);
-  } else {
     win.loadURL(`http://localhost:3000/index.html`);
     win.webContents.openDevTools();
+    electronReload(__dirname, {
+      electron: path.join(__dirname, "..", "..", "node_modules", ".bin", `electron${process.platform === "win32" ? ".cmd" : ""}`),
+      forceHardReset: true,
+      hardResetMethod: "exit",
+    });
+  } else {
+    win.loadURL(`file://${__dirname}/../index.html`);
   }
   win.setResizable(true);
   win.focus();
 };
 
-app.whenReady().then(() => {
-  createWindow();
+app
+  .whenReady()
+  .then(() => {
+    createWindow();
 
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
+    // DevTools
+    installExtension(REACT_DEVELOPER_TOOLS)
+      .then((name: string) => console.log(`Extension : ${name}`))
+      .catch((err) => console.error(`[ERROR]`, err));
+
+    app.on("activate", () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+      }
+    });
+
+    app.on("window-all-closed", () => {
+      if (process.platform !== "darwin") {
+        app.quit();
+      }
+    });
+  })
+  .catch((err) => {
+    console.error(err);
   });
-});
-
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,66 @@
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <h1>HELLO WORLD</h1>
-      <div>My name is YongJoo</div>
-    </header>
-  </div>
-);
+import { KeyboardEvent, useEffect, useRef, useState } from "react";
+
+type TodoItem = {
+  checked: boolean;
+  content: string;
+};
+
+const App = () => {
+  const [todoList, setTodoList] = useState<TodoItem[]>([]);
+
+  useEffect(() => {
+    setTodoList([
+      {
+        checked: true,
+        content: "아침에 일어나기",
+      },
+    ]);
+  }, []);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const onClickWrite = () => {
+    const value = inputRef?.current?.value;
+    if (value) {
+      setTodoList((prev) => [...prev, { checked: false, content: value }]);
+      inputRef.current.value = "";
+    }
+  };
+
+  const onEnterInputTodo = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === `Enter`) {
+      onClickWrite();
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My Todo List</h1>
+      </header>
+      <section>
+        <input type="text" ref={inputRef} onKeyDown={onEnterInputTodo} />
+        <button type="button" onClick={onClickWrite}>
+          작성
+        </button>
+        <ul>
+          {todoList.map((item, index) => {
+            const { checked, content } = item;
+            const id = `CHECKED_${index}`;
+            return (
+              <li key={content}>
+                <div className="bg-red-500 border border-black">
+                  <label htmlFor={id} />
+                  <input type="checkbox" checked={checked} name="" id={id} />
+                  <div>{content}</div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </div>
+  );
+};
 
 export default App;
